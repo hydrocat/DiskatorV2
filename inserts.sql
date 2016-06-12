fc7f45756abd25b83f1c316beea6ca96feb511d8
hydrocat <vdrbandeiras@gmail.com>
2016-06-12 18:49:25 -0300

    testes de insert

---
+++ inserts.sql
@@ -0,0 +1,9 @@
+insert into alunos( ra, idade, nome, sexo) values(
+            1912836,  20, "joao da silaca", 'm'
+);
+insert into empregados( id, nome, salario, posicao) values(
+    124, "alberto", 1200, "Gerente"
+);
+insert into animais( especie, nome, familia) values(
+        "mamifero", "limeira", "canis familiaris"
+);
