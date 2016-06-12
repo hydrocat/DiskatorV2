082b330044b3a8e776af3b9bde4920e6a7d6f1ab
hydrocat <vdrbandeiras@gmail.com>
2016-06-12 14:40:04 -0300

    comentarios

--- comandoSQL.py
+++ comandoSQL.py
@@ -2,7 +2,59 @@ class comandoSQL():
     """
     nomeTabela String
     comando CREATE | INSERT | DELETE
-    dados lista de pares ("nome do campo","Tipo de dado")
+    dados lista de pares ("nome do campo","Tipo de dado", [valor])
+
+    exemplo CREATE:
+        segundo o create a seguir:
+        create table alunos(
+            ra integer,
+            nome varchar(50),
+            sexo char(1),
+            idade integer,
+            altura float
+        )
+
+        o objeto cmoandoSQL montado sera equivalente a:
+            comandoSQL
+                nomeTabela = "alunos"
+                comando = "create"
+                dados = [
+                    [ "ra", "integer" ],
+                    [ "nome", "varchar", 50 ],
+                    [ "sexo", "char", 1],
+                    [ "altura", "float" ]
+                ]
+
+    exemplo INSERT:
+        segundo o insert a seguir:
+            insert into alunos(ra, nome, sexo) values( 
+                    1235,
+                    "vitorio",
+                    'm',
+            )
+
+        o objeto cmoandoSQL montado sera equivalente a:
+            comandoSQL
+                nomeTabela = "alunos"
+                comando = "insert"
+                dados = [
+                    [ "ra", 1235 ],
+                    [ "nome", "vitorio"],
+                    [ "sexo", "m" ]
+                ]
+
+
+    exemplo DELETE:
+        segundo o insert a seguir:
+            delete from alunos where nome = "vitorio"
+
+        o objeto cmoandoSQL montado sera equivalente a:
+            comandoSQL
+                nomeTabela = "alunos"
+                comando = "delete"
+                dados = [
+                    [ "nome", "vitorio" ]
+                ]
     """
 
     def __init__(self, nomeTabela, comando, dados):
