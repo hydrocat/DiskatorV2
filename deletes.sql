4ac25ad4e3ee0aefcfbd11164b25f0e99e428028
hydrocat <vdrbandeiras@gmail.com>
2016-06-12 20:41:05 -0300

    testes de deleção

---
+++ deletes.sql
@@ -0,0 +1,4 @@
+DELETE  FROM  employee WHERE id    = 2;    
+  DELETE    FROM  employee WHERE sex   = 'M';  
+  DELETE    FROM  alunos WHERE jubilado     = True ;  
+  DELETE    FROM  employee  WHERE name  = 'Joao da    Silva';
