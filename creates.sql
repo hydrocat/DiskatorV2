f9b86c4b4554fceef08c4572c178bda8aad80e3f
hydrocat <vdrbandeiras@gmail.com>
2016-06-12 13:45:15 -0300

    testes melhores

---
+++ creates.sql
@@ -0,0 +1,14 @@
+create table employee(
+    id integer,
+    nome varchar(255),
+    sex char(1),
+    status boolean
+);
+
+create table alunos(
+    ra integer,
+    nome varchar(50),
+    sexo char(1),
+    idade integer,
+    altura float
+)
