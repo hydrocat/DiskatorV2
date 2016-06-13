1d7ed7a3e36f6db7f87d342992592953f1f4abe2
hydrocat <vdrbandeiras@gmail.com>
2016-06-13 19:27:35 -0300

    ligacos

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
+from funcoes import *
+import parse
 import menu as m
-import funcoes as f
 import sys
+
+global arqvs
+global valores
+arqvs = dict()
+valores = dict()
 
 def erro():
     print("A opcao selecionada é invalida.\ndigite um numero de 1 a 5") 
 
 def sair():
     sys.exit(0)
+
+def createArquivo():
+    creates = parse("Entre com o nome do arquivo: ") 
+    arquivos = [ arquivo(x.nomeTabela) for x in creates ]
+    for i in range( len(arquivos)):
+        arqvs.update(  { creates[i].nomeTabela :  arquivos[i] })
+        valores.update({ creates[i].nomeTabela :  creates[i]  })
+
+    for tabela in arqvs.keys():
+        arquivos[tabela].createArquivo( valores[tabela].dados )
+        
+def inertRegistro():
+    inserts = parse("Entre com o nome do arquivo: ")
 
 
 if __name__ == "__main__":
@@ -15,5 +34,6 @@ if __name__ == "__main__":
                 2 - Inserir Registro
                 3 - Deletar Registro
                 4 - Listar Registro"""
-    funcoes = [ erro, sair, f.createArquivo, f.insertRegistro, f.deleteRegistro, f.listarRegistro ]
+    
+    funcoes = [ erro, sair, createArquivo, insertRegistro, deleteRegistro, listarRegistro ]
     menu = m.menu(funcoes, string)
